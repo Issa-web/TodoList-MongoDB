d992ad49310c2ee9ae7286ee86d011a61d2bd31c
Issa sangare <issabmsangare@hotmail.com>
2020-12-15 19:26:25 -0500

    last commit

--- app.js
+++ app.js
@@ -149,9 +149,9 @@ app.post("/delete", function(req, res){
 //   res.render("list", {listTitle: "Work List", newListItems: workItems});
 // });
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
