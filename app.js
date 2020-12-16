0c339ec4fa1d91620dd4c86c6b40b35f1cac070d
Issa sangare <issabmsangare@hotmail.com>
2020-12-15 19:22:01 -0500

    use lodash to capitalize only the first letter of our cuustomListName

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash")
 
 
 const app = express();
@@ -72,7 +73,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", (req, res)=>{
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name : customListName}, (err, foundList)=>{
     if(!err){
